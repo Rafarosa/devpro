e2a8d9793cbde5d9643a6e7e2beb54c5929a5565
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2024-01-06 21:41:52 -0300

    trilaha fundamental + Exercícios PythonBR

---
+++ 00 - Fundamentos do Python/python_1_hora.py
@@ -0,0 +1,14 @@
+## Fundamentos do Python
+## Fundamentos de lógica de Programação
+
+## Tipo Inteiro
+print(type(1))
+
+print(1 + 3)
+print(4 - 2)
+
+## Tipo Flutuante (Ponto flutuante)
+print(type(7.9999))
+
+## Cadeia de Caracteres (String)
+print('Olá mundo!!!')
