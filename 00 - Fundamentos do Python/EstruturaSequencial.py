b13e53345f067c966a4501a9d16e2889c5c293ee
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2024-01-12 18:07:01 -0300

    Continua os exercicios

--- 00 - Fundamentos do Python/EstruturaSequencial.py
+++ 00 - Fundamentos do Python/EstruturaSequencial.py
@@ -125,7 +125,6 @@ construa um algoritmo que calcule seu peso ideal,
 utilizando as seguintes fórmulas:
 A) Para homens: (72.7*h) - 58
 B) Para mulheres: (62.1*h) - 44.7
-"""
 altura_pessoa = float(input('Informe sua altura: '))
 
 peso_ideal_h = (72.7 * altura_pessoa) - 58
@@ -133,6 +132,7 @@ peso_ideal_m = (62.1 * altura_pessoa) - 44.7
 
 print(f'Peso ideal para homens é de {peso_ideal_h:.2f}Kg')
 print(f'Peso ideal para mulheres é de {peso_ideal_m:.2f}Kg')
+"""
 
 """
 14 - João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. 
@@ -140,8 +140,20 @@ Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulament
 João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. 
 Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. 
 Imprima os dados do programa com as mensagens adequadas.
+peso = float(input('Informe quantos quilos de peixe você pescou: '))
+regulamento = 50
+excesso = (peso - regulamento) * 4
 
-15 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+if excesso <= 0:
+    print('Sem multa para o pagamento')
+else:
+    print(f'Sua multa é de R${excesso:.2f}')
+"""
+
+"""
+15 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, 
+faça um programa que nos dê:
 A) salário bruto.
 B) quanto pagou ao INSS.
 C) quanto pagou ao sindicato.
@@ -153,7 +165,20 @@ E) calcule os descontos e o salário líquido, conforme a tabela abaixo:
 - Sindicato ( 5%) : R$
 = Salário Liquido : R$
 Obs.: Salário Bruto - Descontos = Salário Líquido.
+"""
+valor_hora_trabalhada = float(input('Informe o valor de sua hora trabalhada: R$'))
+horas_trabalhadas_mes = str(input('Informe as horas trabalhadas no formato (HH:MM): '))
 
+horas, minutos = horas_trabalhadas_mes.split(':')
+horas_trabalhadas_mes_inteiro = (int(horas) + (int(minutos) /60))
+
+salario_bruto = valor_hora_trabalhada * horas_trabalhadas_mes_inteiro
+ir = salario_bruto * 0.11
+inss = salario_bruto * 0.08
+sindicato = 
+
+
+"""
 16 - Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
 17 - Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
